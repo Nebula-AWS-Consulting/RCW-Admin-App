ee7e52cf33a65f42c86afc0e8645c561dd514435
Emmanuel Urias <emmanuelurias60@icloud.com>
2025-03-02 14:38:01 -0800

    Fix persistence and add logout button

--- src/client-admin/src/app.tsx
+++ src/client-admin/src/app.tsx
@@ -7,8 +7,7 @@ import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
 import { ThemeProvider } from 'src/theme/theme-provider';
 
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './store';
+import { store } from './store';
 
 // ----------------------------------------------------------------------
 
@@ -18,9 +17,7 @@ export default function App() {
   return (
     <ThemeProvider>
       <Provider store={store}>
-        <PersistGate persistor={persistor}>
           <Router />
-        </PersistGate>
       </Provider>
     </ThemeProvider>
   );
